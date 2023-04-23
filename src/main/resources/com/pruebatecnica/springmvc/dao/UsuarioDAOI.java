e5090fee6597c048b0713f7c4b94c7dcd54f277d
William Del Cid <dm18019@ues.edu.sv>
2023-04-23 08:40:52 -0600

    Implement methode UsuarioDao in usuarioDAOI

---
+++ src/main/resources/com/pruebatecnica/springmvc/dao/UsuarioDAOI.java
@@ -0,0 +1,69 @@
+package com.pruebatecnica.springmvc.dao;
+import com.pruebatecnica.springmvc.model.Usuarios;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+public class UsuarioDAOI implements UsuarioDao{
+
+    public UsuarioDAOI(DataSource dataSourse){
+        this.jdbcTemplate = new JdbcTemplate(dataSourse);
+    }
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public int guardar(Usuarios usuarios) {
+        String sql = "INSERT INTO \"usuarios\" (\"user\", \"password\") VALUES (?, ?) ";
+        return jdbcTemplate.update(sql,usuarios.getUser(),usuarios.getPassword());
+    }
+    @Override
+    public int actualizar(Usuarios usuarios) {
+       String sql = "UPDATE \"usuarios\" SET \"user\" = ?, \"password\" = ? WHERE \"idUser\" = ? ";
+       return jdbcTemplate.update(sql,usuarios.getUser(),usuarios.getPassword(),usuarios.getIdUser());
+    }
+    @Override
+    public Usuarios obtener(Integer id) {
+        String sql = "SELECT * FROM \"usuarios\" WHERE \"idUser\"=" + id;
+        ResultSetExtractor<Usuarios> extractor = new ResultSetExtractor<Usuarios>() {
+            @Override
+            public Usuarios extractData(ResultSet resultSet) throws SQLException, DataAccessException {
+                if (resultSet.next()){
+                    String user = resultSet.getString("user");
+                    String password = resultSet.getString("password");
+                    return new Usuarios(id,user,password);
+                }
+                return null;
+            }
+        };
+        return jdbcTemplate.query(sql,extractor);
+    }
+
+    @Override
+    public int eliminar(Integer id) {
+        String sql = "DELETE FROM \"usuarios\" WHERE \"idUser\"="+id;
+        return jdbcTemplate.update(sql);
+
+    }
+
+    @Override
+    public List<Usuarios> list() {
+        String sql = "SELECT * FROM \"usuarios\" ";
+        RowMapper<Usuarios> rowMapper = new RowMapper<Usuarios>() {
+            @Override
+            public Usuarios mapRow(ResultSet resultSet, int i) throws SQLException {
+                int id = resultSet.getInt("idUser");
+                String user = resultSet.getString("user");
+                String password = resultSet.getString("password");
+                return new Usuarios(id,user,password);
+            }
+        };
+        return jdbcTemplate.query(sql,rowMapper);
+
+    }
+}
