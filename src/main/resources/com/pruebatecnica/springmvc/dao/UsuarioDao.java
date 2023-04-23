8eb23a1e340157b7a02ba20e3b908479f2bf5522
William Del Cid <dm18019@ues.edu.sv>
2023-04-23 08:40:22 -0600

    Add UsuarioDao in package dao

---
+++ src/main/resources/com/pruebatecnica/springmvc/dao/UsuarioDao.java
@@ -0,0 +1,13 @@
+package com.pruebatecnica.springmvc.dao;
+
+import com.pruebatecnica.springmvc.model.Usuarios;
+
+import java.util.List;
+
+public interface UsuarioDao {
+    public int guardar(Usuarios usuarios);
+    public int actualizar(Usuarios usarios);
+    public Usuarios obtener(Integer id);
+    public int eliminar(Integer id);
+    public List<Usuarios> list();
+}
